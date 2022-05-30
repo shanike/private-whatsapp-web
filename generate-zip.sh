7f535e39e5eea4da60ae016171e8e7086b8472dd
shanike <shani@nadalia.com>
2022-05-30 20:11:27 +0300

    created generate-zip file

---
+++ generate-zip.sh
@@ -0,0 +1,12 @@
+zip ext.zip \
+manifest.json \
+popup/popup.css \
+popup/hide-button.js \
+popup/hide-chat-title-too.js \
+popup/blur-conversation.js \
+popup/default-hide.js \
+popup/popup.html \
+script/script.js \
+script/script.css \
+script/blur-conversations.css \
+icon128.png
