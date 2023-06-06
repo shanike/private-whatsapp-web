2874fd0a15e7f4fc461927b14cc4dc2d90990b0a
shanike <shani@nadalia.com>
2023-06-06 15:59:05 +0300

    updated generate-zip.sh script

--- generate-zip.sh
+++ generate-zip.sh
@@ -1,13 +1,22 @@
-zip ext.zip \
-manifest.json \
-icon128.png \
-popup/blur-conversation.js \
-popup/default-hide.js \
-popup/hide-button.js \
-popup/hide-chat-title-too.js \
-popup/popup.css \
-popup/popup.html \
-script/blur-conversations.css \
-script/script.js \
-script/script.css \
-# rememeber to compile the .scss to .css !!!
+echo
+echo -e "\033[0;31mDid you remember to compile the .scss to .CSC!!?\033[0m"
+echo
+read -p "" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo
+    zip ext.zip \
+    manifest.json \
+    icon128.png \
+    popup/blur-conversation.js \
+    popup/default-hide.js \
+    popup/hide-button.js \
+    popup/hide-chat-title-too.js \
+    popup/popup.css \
+    popup/popup.html \
+    script/blur-conversations.css \
+    script/script.js \
+    script/script.css \
+
+fi
